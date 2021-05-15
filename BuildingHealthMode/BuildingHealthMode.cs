c127c555c62c6c5c299fdd3d9457c729106c9a87
projjm <projjmail@gmail.com>
2021-05-15 16:17:09 +0100

    Updated version number

--- BuildingHealthMode/BuildingHealthMode.cs
+++ BuildingHealthMode/BuildingHealthMode.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 
 namespace BuildingHealthMode
 {
-    [BepInPlugin("projjm.buildingHealthMode", "Building Health Mode", "1.1.0")]
+    [BepInPlugin("projjm.buildingHealthMode", "Building Health Mode", "1.1.1")]
     [BepInProcess("valheim.exe")]
     public class BuildingHealthMode : BaseUnityPlugin
     {
